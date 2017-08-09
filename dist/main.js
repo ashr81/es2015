28de56b76ab14f577fb847b5a2b9c04f72322519
ashr81-rg <ashrith.vontari@reportgarden.com>
2017-08-09 23:03:44 +0530

    number and string methods.

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,19 @@
 "use strict";
 
-var name = "Ashrith";
-function makeUpperCase(word) {
-  return word.toUpperCase();
-}
-var templateLiteral = '<div>Hello ${name}</div>' + '<p>I am not text.</p>' + ("<div>How string literal is used?</div>\n<p>This is how it's used for new line and large html elements</p>\n<span>No need of any concatination.</span>\n<p> name is " + name + " in upcase " + makeUpperCase('Javascript') + "</p>");
-document.getElementById('template').innerHTML = templateLiteral;
+var theString = 'Hello ,this is testing sentence.';
+
+console.log(theString.startsWith('Hello')); // true
+console.log(theString.endsWith('sentence.')); // true
+console.log(theString.includes(' is')); // true
+
+//Hex
+console.log(0xFF); // 255
+// Binary
+console.log(20); // 20
+// Octal
+console.log(355); // 355
+console.log(Number.isFinite(12)); // true
+console.log(Number.isFinite(Infinity)); // false
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.isInteger(2)); // true
+console.log(Number.isInteger(Infinity)); // false
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 "use strict"
 
+let theString = 'Hello ,this is testing sentence.'
 
-let name = "Ashrith";
-function makeUpperCase(word){
-  return word.toUpperCase();
-}
-let templateLiteral =
-'<div>Hello ${name}</div>'+
-'<p>I am not text.</p>'+
-`<div>How string literal is used?</div>
-<p>This is how it's used for new line and large html elements</p>
-<span>No need of any concatination.</span>
-<p> name is ${name} in upcase ${makeUpperCase('Javascript')}</p>`
-document.getElementById('template').innerHTML = templateLiteral;
+console.log(theString.startsWith('Hello')) // true
+console.log(theString.endsWith('sentence.')) // true
+console.log(theString.includes(' is')) // true
+
+//Hex
+console.log(0xFF); // 255
+// Binary
+console.log(0b10100); // 20
+// Octal
+console.log(0o543); // 355
+console.log(Number.isFinite(12)); // true
+console.log(Number.isFinite(Infinity)); // false
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.isInteger(2)) // true
+console.log(Number.isInteger(Infinity)) // false
