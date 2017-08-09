d57430cf727d5522d1342dfc97c532bf8a686cce
ashr81-rg <ashrith.vontari@reportgarden.com>
2017-08-09 22:11:39 +0530

    First commit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+"use strict"
+/*
+function testVar(){
+  var a = 30;
+  if(true){
+    var a = 50;
+    console.log(a);
+  }
+  console.log(a)
+}
+
+function testLet(){
+  let a = 30;
+  if(true){
+    let a = 50;
+    console.log(a)
+  }
+  console.log(a)
+}
+testVar();
+// print 50 and 50
+testLet();
+// print 50 and 30
+
+for(var i = 0; i < 10; i++ ){
+  console.log(i)
+}
+console.log(i); // print this print 10
+
+for(let i = 0; i < 10; i++ ){
+  console.log(i);
+}
+console.log(i); // its undefined here.
+
+const colors = [];
+colors.push('red');
+colors.push('blue');
+
+colors = 'Green'; // throws an error. can't change the value itself. but can add into it.
+
+console.log(colors);
+*/
