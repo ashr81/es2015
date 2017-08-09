ec671a0ba99a08a1d388d2910a638d14869b6403
ashr81-rg <ashrith.vontari@reportgarden.com>
2017-08-09 23:39:37 +0530

    set, map.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,37 @@
 "use strict"
 
-// Default Parameters
-function greet(greeting= 'Hello World'){
-  console.log(greeting)
+// Set
+let myArray = [54, 43, 54, 54, 32, 54];
+let mySet = new Set(myArray);
+console.log(mySet) // print {54, 43, 32}
+mySet.add('100')
+mySet.add({a: 100, b: 2})
+console.log(mySet) // print {54, 43, 32, '100', {a: 100, b: 2}}
+
+mySet.forEach(function(val){
+  console.log(val);
+})
+
+
+// Map
+let myMap = new Map([['a1', 'g1'], ['b1', 'h1']]);
+console.log(myMap); // {"a1" => "g1", "b1" => "h1"}
+myMap.set("c1", "i1");
+myMap.delete("a1");
+console.log(myMap); // {"b1" => "h1", "c1" => "i1"}
+
+
+// WeakSet
+let carWeakSet = new WeakSet();
+let car1 = {
+  make: 'Honda',
+  model: 'Civic'
 }
-greet();
-
-// Spread operator.
-
-let args = [1, 2, 3];
-
-function test(){
-  console.log(args);
+carWeakSet.add(car1);
+let car2 = {
+  make: 'Toyota',
+  model: 'Camry'
 }
-test.apply(args);
-test(...args);
+carWeakSet.add(car2);
+carWeakSet.delete(car1);
+console.log(carWeakSet) // {{make: 'Toyota', model: 'Camry'}}
