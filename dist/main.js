276534782ef464fb32553f665d702efab75c4936
ashr81-rg <ashrith.vontari@reportgarden.com>
2017-08-09 23:15:14 +0530

    Default param and spread operator.

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,20 @@
 "use strict";
 
-var theString = 'Hello ,this is testing sentence.';
+// Default Parameters
 
-console.log(theString.startsWith('Hello')); // true
-console.log(theString.endsWith('sentence.')); // true
-console.log(theString.includes(' is')); // true
+function greet() {
+  var greeting = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Hello World';
 
-//Hex
-console.log(0xFF); // 255
-// Binary
-console.log(20); // 20
-// Octal
-console.log(355); // 355
-console.log(Number.isFinite(12)); // true
-console.log(Number.isFinite(Infinity)); // false
-console.log(Number.isNaN(NaN)); // true
-console.log(Number.isInteger(2)); // true
-console.log(Number.isInteger(Infinity)); // false
+  console.log(greeting);
+}
+greet();
+
+// Spread operator.
+
+var args = [1, 2, 3];
+
+function test() {
+  console.log(args);
+}
+test.apply(args);
+test.apply(undefined, args);
--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 "use strict"
 
-let theString = 'Hello ,this is testing sentence.'
+// Default Parameters
+function greet(greeting= 'Hello World'){
+  console.log(greeting)
+}
+greet();
 
-console.log(theString.startsWith('Hello')) // true
-console.log(theString.endsWith('sentence.')) // true
-console.log(theString.includes(' is')) // true
+// Spread operator.
 
-//Hex
-console.log(0xFF); // 255
-// Binary
-console.log(0b10100); // 20
-// Octal
-console.log(0o543); // 355
-console.log(Number.isFinite(12)); // true
-console.log(Number.isFinite(Infinity)); // false
-console.log(Number.isNaN(NaN)); // true
-console.log(Number.isInteger(2)) // true
-console.log(Number.isInteger(Infinity)) // false
+let args = [1, 2, 3];
+
+function test(){
+  console.log(args);
+}
+test.apply(args);
+test(...args);
